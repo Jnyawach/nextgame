068c0e8060c88812dcffd5c0d80455009bfafe58
jnyawach <nyawach41@gmail.com>
2022-11-06 09:28:24 +0300

    tag

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -9,6 +9,13 @@
     <link href="{{asset('css/fontawesome/css/all.css')}}" rel="stylesheet">
     <link rel = "icon" href ="{{asset('images/kola-icon.png')}}" type = "image/x-icon">
     @yield('styles')
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-TNVRW6J');</script>
+    <!-- End Google Tag Manager -->
     <script src="{{asset('js/js.cookie.js')}}"></script>
     <script>
         var time=Intl.DateTimeFormat().resolvedOptions().timeZone
@@ -17,6 +24,10 @@
    @livewireStyles
 </head>
 <body>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TNVRW6J"
+                  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 <div class="content">
     <header class="fixed-top">
         <!---extended navbar for bigger menus-->
